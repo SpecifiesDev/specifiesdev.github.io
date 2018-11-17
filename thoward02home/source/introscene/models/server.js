ad517d9a6076b06396c0fe37b06871c464a9e5ec
SpecifiesDev <specifiesdev@gmail.com>
2018-11-17 01:58:36 -0500

    Add the WebGL project home to repository
    Basically, testing a server I wrote to help him bypass CORS errors.

---
+++ thoward02home/source/introscene/models/server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+  const { headers, method, url } = request;
+  let body = [];
+  request.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    
+
+    response.on('error', (err) => {
+      console.error(err);
+    });
+    response.statusCode = 200;
+    console.log(request.headers)
+    response.setHeader('Access-Control-Allow-Origin', request.headers['origin']);
+    response.setHeader("Access-Control-Allow-Credentials", true);
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    response.setHeader("Acces-Control-Max-Age", "3600");
+	response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+    // set the header to bypass CORS
+
+    const responseBody = { headers, method, url, body };
+
+    response.write(JSON.stringify(responseBody));
+    response.end();
+
+  });
+}).listen(8080);
+console.log("Server started on port 8080");
