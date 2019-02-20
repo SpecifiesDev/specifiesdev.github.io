1f1072c76529df00a4375c194667102ae3c0eebe
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2019-02-20 13:29:12 -0500

    Code Format OH | Coin Logic (Broken)

--- moleio/scripts/game.js
+++ moleio/scripts/game.js
@@ -1,25 +1,26 @@
-	// main loop
+    // main loop
+    var canvas = document.getElementById("canv");
+    var ctx = canvas.getContext("2d");
+    ctx.canvas.width = 1225;
+    ctx.canvas.height = 875;
 
+    document.onkeydown = keyDownHandler;
+    document.onkeyup = keyUpHandler;
 
-	var canvas = document.getElementById("canv");
-	var ctx = canvas.getContext("2d");
-	ctx.canvas.width = 1225;
-	ctx.canvas.height = 875;
+    var tX = -(window.innerWidth - 11);
+    var tY = 830;
 
-	document.onkeydown = keyDownHandler;
-	document.onkeyup = keyUpHandler;
+    var testGrass = grass({
+        x: 0,
+        y: 818
+    });
 
-	var tX = -(window.innerWidth - 11);
-	var tY = 830;
-	
-	var testGrass = grass({x: 0, y: 818});
+    var level1 = true;
+    var l1s = false;
 
-	var level1 = true;
-	var l1s = false;
+    var loadedLevel = [];
 
-	var loadedLevel = [];
-
-	var speed = 11;
+    var speed = 11;
 
 
 
@@ -30,9 +31,9 @@
 
     var upKey = [];
 
-   	var upandRight = [];
+    var upandRight = [];
 
-   	var upandLeft = [];
+    var upandLeft = [];
 
     var bullets = [];
 
@@ -43,36 +44,42 @@
     var collidingX = 0;
     var collidingY = 0;
 
-	function update() {
+    var coinv = 0;
 
-        
+    function update() {
+
+
         seconds = Math.floor((Date.now() - start) / 1000);
-        if(seconds % 5 == 0) {
+        if (seconds % 5 == 0) {
             gunObjects.forEach(function(e) {
-                bullets.push(bullet({x: e.x - 10, y: e.y + 15, dx: 3}));
+                bullets.push(bullet({
+                    x: e.x - 10,
+                    y: e.y + 15,
+                    dx: 3
+                }));
             });
         }
 
-        bullets.forEach(function (e) {
+        bullets.forEach(function(e) {
 
             e.x -= e.dx;
 
         });
-        
-		
-		move();
-		if(inAir(player)) {
-          if(!playerVerticalCollision()) {
-		      player.y += player.dy;
-		      player.x += player.dx;
-          } else if(playerVerticalCollision()) {
-            player.dy = 0;
-            player.dx = 0;
-            upKey = [];
-            upandRight = [];
-            upandLeft = [];
-          }
-        } else { 
+
+
+        move();
+        if (inAir(player)) {
+            if (!playerVerticalCollision()) {
+                player.y += player.dy;
+                player.x += player.dx;
+            } else if (playerVerticalCollision()) {
+                player.dy = 0;
+                player.dx = 0;
+                upKey = [];
+                upandRight = [];
+                upandLeft = [];
+            }
+        } else {
             player.dy = 0;
             player.dx = 0;
             upKey = [];
@@ -80,118 +87,155 @@
             upandLeft = [];
         }
 
-	}
+    }
 
-	function draw() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		if(level1 && !l1s) {
-			loadLevel(l1);
-			l1s = true;
-			console.log("Level one loaded!");
-		}
-		
-		loadedLevel.forEach(function(e) {
-            if(e.type == "grass" || e.type == "rock" || e.type == "gun" || e.type == "bottom") {
+        if (level1 && !l1s) {
+            loadLevel(l1);
+            l1s = true;
+            console.log("Level one loaded!");
+        }
+
+        loadedLevel.forEach(function(e) {
+            if (e.type == "grass" || e.type == "rock" || e.type == "gun" || e.type == "bottom" || e.type == "coin") {
                 regularObjects.push(e);
             }
-			e.draw();
-		});
-		player.draw();
+            e.draw();
+        });
+        player.draw();
         bullets.forEach(function(e) {
-            if(e.x < 0) {
+            if (e.x < 0) {
                 bullets.pop(e);
             } else {
                 e.draw();
             }
         });
 
+        drawCoin();
+    }
 
-	}
+    var cursor = {
+        'rightPressed': false,
+        'leftPressed': false,
+        'downPressed': false,
+        'upPressed': false,
+        'r': false
+    }
+    var player = {
+        color: "#5f708c",
+        x: 11,
+        y: 830,
+        dy: 0,
+        dx: 0,
+        imageEnabled: false,
+        image: "/",
+        draw: function() {
+            if (this.imageEnabled) {
 
-	var cursor = {
-		'rightPressed' : false,
-		'leftPressed' : false,
-		'downPressed' : false,
-		'upPressed' : false,
-		'r' : false
-	}
-	var player = {
-		color: "#5f708c",
-		x: 11,
-		y: 830,
-		dy: 0,
-		dx: 0,
-		imageEnabled: false,
-		image: "/",
-		draw : function() {
-			if(this.imageEnabled) {
+            } else if (!this.imageEnabled) {
+                // If we don't have a sprite, place a circle for testing purpose
+                ctx.beginPath();
+                ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
+                ctx.fillStyle = this.color;
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+    var coins = {
+        color: "#5f708c",
+        x: 1100,
+        y: 30,
+        drawg: function() {
+            createText(coinv, this.x, this.y, "15px x", this.color);
+        }
+    }
 
-			} else if(!this.imageEnabled) {
-				// If we don't have a sprite, place a circle for testing purpose
-				ctx.beginPath();
-	            ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
-	            ctx.fillStyle = this.color;
-	            ctx.fill();
-	            ctx.closePath();
-			}
-		}
-	}
     function playerVerticalCollision() {
         var breakv = false;
 
-        if(player.y <= 830) {
+        if (player.y <= 830) {
             regularObjects.forEach(function(e) {
-                if(e.type == "grass" || e.type == "gun" || e.type == "rock") {
-                    if(!breakv) {
-                        if(collides(player, e)) {
+                if (e.type == "grass" || e.type == "gun" || e.type == "rock") {
+                    if (!breakv) {
+                        if (collides(player, e)) {
                             collidingY = e.y;
                             collidingX = e.x;
                             breakv = true;
 
-                        } 
+                        }
+                    }
                 }
-            }
-        });
+                if (e.type == "coin") {
+                    if (collides(player, e)) {
+                        loadedLevel.pop(e);
+                        loadedLevel.push(sky({
+                            x: e.x,
+                            y: e.y,
+                            type: "sky"
+                        }));
+                        coinv += 1;
+                    }
+                }
+            });
         }
         return breakv;
 
     }
+
     function playerHorizontalCollision() {
         var breakv = false;
-        if(!inAir(player)) {
+        if (!inAir(player)) {
             console.log(player.y);
-            if(player.y <= 830) {
+            if (player.y <= 830) {
                 regularObjects.forEach(function(e) {
-                    if(e.type == "grass" || e.type == "gun" || e.type == "rock" || e.type == "bottom") {
-                        if(!breakv) {
-                            if(collides(player, e)) {
+                    if (e.type == "grass" || e.type == "gun" || e.type == "rock" || e.type == "bottom") {
+                        if (!breakv) {
+                            if (collides(player, e)) {
                                 collidingY = e.y;
                                 collidingX = e.x;
                                 breakv = true;
-                            } 
-                    }
+                            }
+                        }
 
-                }
-            });
+                    }
+                    if (e.type == "coin") {
+                        if (collides(player, e)) {
+                            loadedLevel.pop(e);
+                            loadedLevel.push(sky({
+                                x: e.x,
+                                y: e.y,
+                                type: "sky"
+                            }));
+                            coinv += 1;
+                        }
+                    }
+                });
             }
         }
         return breakv;
 
     }
-	// 13 represents the size of our player object. 
-    function collides(a, b) { return a.x < b.x + 35 &&
-   a.x + 10 > b.x &&
-   a.y < b.y + 35 &&
-   a.y + 10 > b.y }
+    // 13 represents the size of our player object. 
+    function collides(a, b) {
+        return a.x < b.x + 35 &&
+            a.x + 10 > b.x &&
+            a.y < b.y + 35 &&
+            a.y + 10 > b.y
+    }
 
-	// 2dc doesn't have a camera object, so we create a function to translate the canvas and undraw objects < 500px
-	function moveCamera(x, y) {
+    // 2dc doesn't have a camera object, so we create a function to translate the canvas and undraw objects < 500px
+    function moveCamera(x, y) {
         tY += y;
         tX += x;
-		ctx.translate(x, y)
+        ctx.translate(x, y)
+        var toCreate = coins.x + x;
+        console.log(toCreate);
+        coin.x = toCreate;
 
-	}
+    }
 
     function keyDownHandler(e) {
         e.preventDefault();
@@ -203,7 +247,7 @@
             cursor.upPressed = true;
         } else if (e.keyCode == 40) {
             cursor.downPressed = true;
-        } else if(e.keyCode ==  82) {
+        } else if (e.keyCode == 82) {
             cursor.r = true;
         }
 
@@ -220,43 +264,46 @@
             cursor.upPressed = false;
         } else if (e.keyCode == 40) {
             cursor.downPressed = false;
-        } else if(e.keyCode == 82) {
+        } else if (e.keyCode == 82) {
             cursor.r = true;
         }
     }
 
     function grass(i) {
-    	var grassImage = new Image(5, 5);
-	 	var imgPath = "./resources/th.jpg"
-	 	grassImage.src = imgPath;
-	 	var w = 35;
-	 	var h = 35; 
-	 	i.draw = function() {
-	 		ctx.drawImage(grassImage, i.x, i.y, w, h);
-	 	}
-	return i;
- 	}
- 	function sky(i) {
- 		var w = 35;
- 		var h = 35;
- 		i.draw = function() {
- 			ctx.beginPath();
- 			ctx.fillStyle = "#769fe0";
- 			ctx.fillRect(i.x, i.y, w, h);
- 			ctx.closePath();
- 		}
- 		return i;
- 	}
- 	function rock(i) {
- 		var rockImage = new Image(5, 5);
- 		rockImage.src = "./resources/rock.png";
- 		var w = 35;
- 		var h = 35;
- 		i.draw = function() {
- 			ctx.drawImage(rockImage, i.x, i.y, w, h);
- 		}
- 		return i;
- 	}
+        var grassImage = new Image(5, 5);
+        var imgPath = "./resources/th.jpg"
+        grassImage.src = imgPath;
+        var w = 35;
+        var h = 35;
+        i.draw = function() {
+            ctx.drawImage(grassImage, i.x, i.y, w, h);
+        }
+        return i;
+    }
+
+    function sky(i) {
+        var w = 35;
+        var h = 35;
+        i.draw = function() {
+            ctx.beginPath();
+            ctx.fillStyle = "#769fe0";
+            ctx.fillRect(i.x, i.y, w, h);
+            ctx.closePath();
+        }
+        return i;
+    }
+
+    function rock(i) {
+        var rockImage = new Image(5, 5);
+        rockImage.src = "./resources/rock.png";
+        var w = 35;
+        var h = 35;
+        i.draw = function() {
+            ctx.drawImage(rockImage, i.x, i.y, w, h);
+        }
+        return i;
+    }
+
     function gun(i) {
         var gunImage = new Image(5, 5);
         gunImage.src = "./resources/gun.jpg";
@@ -267,6 +314,7 @@
         }
         return i;
     }
+
     function bullet(i) {
         i.draw = function() {
             ctx.beginPath();
@@ -274,9 +322,10 @@
             ctx.fillStyle = "#204a8e";
             ctx.fill();
             ctx.closePath();
-        } 
+        }
         return i;
     }
+
     function bottom(i) {
         var bottomImage = new Image(5, 5);
         bottomImage.src = "./resources/bottom.jpg";
@@ -287,6 +336,7 @@
         }
         return i;
     }
+
     function cloud(i) {
         var w = 35;
         var h = 35;
@@ -298,51 +348,103 @@
         }
         return i;
     }
- 	function loadLevel(toLoad) {
- 		loadedLevel = [];
- 		for(var c = 0; c < toLoad.cols; c++) {
- 			for(var r = 0; r < toLoad.rows; r++) {
- 				var tile = toLoad.getTile(c, r);
- 				if(tile == 1) {
- 					loadedLevel.push(grass({x: c * 35, y: r * 35, type: "grass"}));
- 				}
- 				if(tile == 0) {
- 					loadedLevel.push(sky({x: c * 35, y: r * 35, type: "sky"}));
- 				}
- 				if(tile == 2) {
- 					loadedLevel.push(rock({x: c * 35, y: r * 35, type: "rock"}));
- 				}
-                if(tile == 3) {
-                    loadedLevel.push(gun({x: c * 35, y: r * 35, type: "gun"}));
-                    gunObjects.push(gun({x: c * 35, y: r * 35}));
+
+    function coin(i) {
+        var coinImage = new Image(5, 5);
+        coinImage.src = "./resources/coin.png";
+        var w = 35;
+        var h = 35;
+        i.draw = function() {
+            ctx.drawImage(coinImage, i.x, i.y, w, h);
+        }
+        return i;
+    }
+
+    function loadLevel(toLoad) {
+        loadedLevel = [];
+        for (var c = 0; c < toLoad.cols; c++) {
+            for (var r = 0; r < toLoad.rows; r++) {
+                var tile = toLoad.getTile(c, r);
+                if (tile == 1) {
+                    loadedLevel.push(grass({
+                        x: c * 35,
+                        y: r * 35,
+                        type: "grass"
+                    }));
                 }
-                if(tile == 4) {
-                    loadedLevel.push(bottom({x: c * 35, y: r * 35, type: "bottom"}));
+                if (tile == 0) {
+                    loadedLevel.push(sky({
+                        x: c * 35,
+                        y: r * 35,
+                        type: "sky"
+                    }));
                 }
-                if(tile == 5) {
-                    loadedLevel.push(cloud({x: c * 35, y: r * 35, type: "cloud"}));
+                if (tile == 2) {
+                    loadedLevel.push(rock({
+                        x: c * 35,
+                        y: r * 35,
+                        type: "rock"
+                    }));
                 }
- 			}
- 		}
- 	}
+                if (tile == 3) {
+                    loadedLevel.push(gun({
+                        x: c * 35,
+                        y: r * 35,
+                        type: "gun"
+                    }));
+                    gunObjects.push(gun({
+                        x: c * 35,
+                        y: r * 35
+                    }));
+                }
+                if (tile == 4) {
+                    loadedLevel.push(bottom({
+                        x: c * 35,
+                        y: r * 35,
+                        type: "bottom"
+                    }));
+                }
+                if (tile == 5) {
+                    loadedLevel.push(cloud({
+                        x: c * 35,
+                        y: r * 35,
+                        type: "cloud"
+                    }));
+                }
+                if (tile == 6) {
+                    loadedLevel.push(coin({
+                        x: c * 35,
+                        y: r * 35,
+                        type: "coin"
+                    }));
+                }
+            }
+        }
+    }
 
     function returnRandom(min, max) {
-    	return Math.floor(Math.random() * max) + min;
+        return Math.floor(Math.random() * max) + min;
     }
-    function inAir(toCheck) {
-    	if(toCheck.y == 830 || toCheck.y > 831) {
-    		return false;
-    	} else {
-    		return true;
-    	}
-    }
-    function move() {
-    	// smoother movement mechanics
-    	if (cursor.rightPressed && cursor.upPressed && player.x < 100 * 35 - 10 && player.y > 10){
-    		upandRight.push("uar");
-    		if(upandRight.length < 10) {
 
-                if(!playerHorizontalCollision()) {
+    function drawCoin() {
+        coins.drawg();
+    }
+
+    function inAir(toCheck) {
+        if (toCheck.y == 830 || toCheck.y > 831) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function move() {
+        // smoother movement mechanics
+        if (cursor.rightPressed && cursor.upPressed && player.x < 100 * 35 - 10 && player.y > 10) {
+            upandRight.push("uar");
+            if (upandRight.length < 10) {
+
+                if (!playerHorizontalCollision()) {
                     player.x += speed;
                     player.y -= speed;
                     player.dy = 4;
@@ -352,80 +454,81 @@
 
                     var l1Limit = 1000 * 35;
 
-                    if(level1) {
+                    if (level1) {
 
                         var swi = l1Limit - player.x < 1172;
 
-                        if(!swi) {
+                        if (!swi) {
                             moveCamera(-10, 0);
                         }
                     }
                 }
-    		}
-    	}
-    	else if (cursor.leftPressed && cursor.upPressed && player.x > 10 && player.y > 10){
-    		upandLeft.push("ual");
-    		if(upandLeft.length < 10) {
-    			player.x -= speed;
-    			player.y -= speed;
-    			player.dy = 4;
-    			player.dx = -2;
+            }
+        } else if (cursor.leftPressed && cursor.upPressed && player.x > 10 && player.y > 10) {
+            upandLeft.push("ual");
+            if (upandLeft.length < 10) {
+                player.x -= speed;
+                player.y -= speed;
+                player.dy = 4;
+                player.dx = -2;
 
                 var limit = 600;
                 var l1Limit = 49 * 35;
                 var swi = player.x > 35;
-                if(level1) {
-                    if(swi) {
+                if (level1) {
+                    if (swi) {
                         moveCamera(10, 0);
                     }
                 }
 
-    		}
-    	}
-        else if (cursor.rightPressed && player.x < 100 * 35 - 10) {
+            }
+        } else if (cursor.rightPressed && player.x < 100 * 35 - 10) {
             console.log(playerHorizontalCollision());
-            if(!playerHorizontalCollision()) {
+            if (!playerHorizontalCollision()) {
                 player.x += speed + 3;
                 var tXI = tX;
 
                 var l1Limit = 100 * 35;
 
-                if(level1) {
+                if (level1) {
                     var swi = l1Limit - player.x < 1000;
-                    if(!swi) {
+                    if (!swi) {
                         moveCamera(-10, 0);
                     }
 
                 }
             }
-        } 
-        else if (cursor.leftPressed && player.x > 10) {
+        } else if (cursor.leftPressed && player.x > 10) {
             player.x -= speed + 3;
             var limit = 600;
             var l1Limit = 49 * 35;
             var swi = player.x > 800;
-            if(level1) {
-                if(swi) {
+            if (level1) {
+                if (swi) {
                     moveCamera(10, 0);
                 }
             }
-        } 
-        else if (cursor.upPressed && player.y > 10) {
-        	upKey.push("up");
-        	if(upKey.length < 10) {
-        		player.y -= speed;
-             	player.dy = 4;
-        	}
-        } 
+        } else if (cursor.upPressed && player.y > 10) {
+            upKey.push("up");
+            if (upKey.length < 10) {
+                player.y -= speed;
+                player.dy = 4;
+            }
+        }
+    }
+
+    function createText(text, x, y, font, color) {
+        ctx.beginPath();
+        ctx.font = font;
+        ctx.fillStyle = color;
+        ctx.fillText(text, x, y);
+        ctx.fill();
+        ctx.closePath();
     }
 
 
 
     var startGame = setInterval(function() {
-    	update();
-    	draw();
+        update();
+        draw();
     }, 1000 / 30);
-
-
-
-
