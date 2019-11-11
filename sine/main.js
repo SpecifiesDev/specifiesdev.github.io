685e3bc4fa9a1dd36a2cf93dfed1cddec403b03d
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2019-11-11 09:11:51 -0500

    Add files via upload

--- sine/main.js
+++ sine/main.js
@@ -6,11 +6,33 @@ let ctx = canvas.getContext("2d");
 let width = ctx.canvas.width;
 let height = ctx.canvas.width;
 
+let interval;
+
 // Just use some jquery for the onclick because vanilla events are disgusting
 $(() => {
+
+    $(document).ready(() => {
+        drawAxes();
+        graphSine(5, 5, 50, 35, "a b c d e f g h i j k l");
+    });
+
     $(".navbar-github").click(() => {
         window.open("https://github.com/SpecifiesDev");
-    })
+    });
+
+    $(".checkbox-type").click(() => {
+        
+        let state = $(".checkbox-type").is(":checked");
+
+        if(state) {
+            interval = setInterval(() => {
+                draw();
+            }, 100);
+        } else if(!state) {
+            clearInterval(interval);
+        }
+        
+    });
 });
 
 // Kind of self explanatory
@@ -35,7 +57,7 @@ function drawAxes() {
 }
 
 /**
- * graphPoint(); Function to graph a point on the sine wave.
+ * graphPoint(); Function to graph a point on a sine wave.
  * @param radius Radius of the point
  * @param x x position of the point on the canvas 
  * @param y y poisition of the point on the canvas 
@@ -53,7 +75,7 @@ function graphPoint(radius, x, y) {
 }
 
 /**
- * graphSine();
+ * graphSine(); Function to graph a sine wave.
  * @param xOffset Amount to offset the x coordinates
  * @param yOffset Amount to offset the y coordinates
  * @param amplitude Amplitude of the waves
@@ -62,6 +84,8 @@ function graphPoint(radius, x, y) {
  */
 function graphSine(xOffset, yOffset, amplitude, frequency, text) {
 
+    console.log(`sine{amp: ${amplitude} freq: ${frequency}}`);
+    
     ctx.beginPath();
 
     ctx.lineWidth = 2;
@@ -127,9 +151,37 @@ function graphSine(xOffset, yOffset, amplitude, frequency, text) {
 
 }
 
+let textToAnimate = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+let xoff = 1;
+let yoff = 1;
+let amp = 5;
+let freq = 2;
 
-drawAxes();
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let lastCharacter = textToAnimate.charAt(textToAnimate.length - 1);
+    textToAnimate = textToAnimate.substring(0, textToAnimate.length - 2);
+
+    textToAnimate = `${lastCharacter} ${textToAnimate}`;
+
+    
+
+    if(amp > 160) {
+        amp = Math.floor(Math.random() * 50) + 1;
+    }
+    if(freq > 170) {
+        freq = Math.floor(Math.random() * 50) + 1;  
+    }
+
+    amp += Math.floor(Math.random() * 3) + 1;
+    freq += Math.floor(Math.random() * 3) + 1;
+
+    drawAxes();
+    graphSine(xoff, yoff, amp, freq, textToAnimate);
+    
+}
 
 
-graphSine(5, 5, 50, 35, "a b c d e f g h i j k l");
+
 
