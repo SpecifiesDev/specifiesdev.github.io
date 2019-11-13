65665640974d8352dd6af874d77380c1d454fcce
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2019-11-13 14:50:01 -0500

    Add files via upload

--- gatsby/assets/js/control.js
+++ gatsby/assets/js/control.js
@@ -1,13 +1,18 @@
 import { setData } from "https://specifiesdev.github.io/gatsby/assets/js/lib.js";
 
-
+// on ready, begin
 $(() => {
 
+    // Events
     $(".navbar-github").click(() => {
         window.open("https://github.com/SpecifiesDev");
     });
 
-
+    // Check and make sure session storage has the path value
+    if(sessionStorage.path == null || "") {
+        setData();
+    }
+    $(".profile-control").attr("src", sessionStorage.path); 
 
 
     
--- gatsby/assets/js/lib.js
+++ gatsby/assets/js/lib.js
@@ -1,11 +1,7 @@
 export function setData() {
-
     let paths = ["./assets/images/profile.jpg", "./assets/images/profile1.jpg"];
-
-    let item = paths[Math.floor(Math.random() * paths.length)];
-
-    sessionStorage.path = item;
-
-    console.log(sessionStorage.path);
-
+    sessionStorage.path = paths[Math.floor(Math.random() * paths.length)];
 }
+
+
+
