870687bed8013544362f6ab9c17a62f35c24e3d6
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2019-11-13 12:41:47 -0500

    Add files via upload

---
+++ lib.js
@@ -0,0 +1,13 @@
+
+
+export function setData() {
+
+    let paths = ["./assets/images/profile.jpg", "./assets/images/profile1.jpg"];
+
+    let item = paths[Math.floor(Math.random() * paths.length)];
+
+    sessionStorage.path = item;
+
+    console.log(sessionStorage.path);
+
+}
