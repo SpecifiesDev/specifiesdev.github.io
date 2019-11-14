a19aac743e705e06d317e42beffe5e1d243e6258
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2019-11-14 05:11:31 -0500

    Add files via upload

--- gatsby/assets/js/lib.js
+++ gatsby/assets/js/lib.js
@@ -3,7 +3,12 @@ export function setData() {
     let preformat = "https://specifiesdev.github.io/gatsby/assets/images/";
 
     let paths = [`${preformat}profile.jpg`, `${preformat}profile1.jpg`];
-    sessionStorage.path = paths[Math.floor(Math.random() * paths.length)];
+
+    let path = paths[Math.floor(Math.random() * paths.length)];
+
+    console.log(`setData(); : ${path}`);
+
+    sessionStorage.path = path;
 }
 
 
