69906533fce9edde0e95ae91ec11b97a047ee53d
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2019-11-14 05:22:30 -0500

    Add files via upload

--- gatsby/assets/js/lib.js
+++ gatsby/assets/js/lib.js
@@ -2,7 +2,17 @@ export function setData() {
     
     let preformat = "https://specifiesdev.github.io/gatsby/assets/images/";
 
-    let paths = [`${preformat}profile.jpg`, `${preformat}profile1.jpg`];
+    let paths = [];
+
+    // Let's just loop through and add the number to make life a little easier
+    for(let i = 0; i < 4; i++) {
+        if(i == 0) {
+            paths.push(`${preformat}profile.jpg`);
+        } else {
+            paths.push(`${preformat}profile${x}.jpg`);
+        }
+        console.log("Push");
+    }
 
     let path = paths[Math.floor(Math.random() * paths.length)];
 
